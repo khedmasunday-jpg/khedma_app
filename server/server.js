6583642d98d10b3b977dd3f431b36af37f6c93bc
khedmasunday-jpg <khedmasunday@gmail.com>
2026-08-14 17:06:52 +0300

    Fix CORS policy to allow x-device-id header

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.set('trust proxy', 1);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-CSRF-Token, Accept, Accept-Version, Date, X-Api-Version');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-CSRF-Token, Accept, Accept-Version, Date, X-Api-Version, x-device-id');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).send();
